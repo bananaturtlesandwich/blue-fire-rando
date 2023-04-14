12f665e78cd23ecdd8dbffabbdd5155fc98102bf
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-04-14 16:40:11 +0100

    make cutscene writing multithreaded

--- src/writing/cutscenes.rs
+++ src/writing/cutscenes.rs
@@ -1,23 +1,28 @@
 use super::*;
 
 pub fn write(cutscenes: Vec<Check>, app: &crate::Rando, pak: &unpak::Pak) -> Result<(), Error> {
-    for Check { context, drop, .. } in cutscenes {
-        let Context::Cutscene(cutscene) = context else {
-            return Err(Error::Assumption);
-        };
-        create_hook(
-            app,
-            &pak,
-            |_| {
-                Ok(open_from_bytes(
-                    include_bytes!("../blueprints/hook.uasset"),
-                    include_bytes!("../blueprints/hook.uexp"),
-                )?)
-            },
-            &drop,
-            cutscene,
-            69,
-        )?
-    }
+    std::thread::scope(|thread| {
+        for Check { context, drop, .. } in cutscenes {
+            thread.spawn(move || -> Result<(), Error> {
+                let Context::Cutscene(cutscene) = context else {
+                    return Err(Error::Assumption);
+                };
+                create_hook(
+                    app,
+                    &pak,
+                    |_| {
+                        Ok(open_from_bytes(
+                            include_bytes!("../blueprints/hook.uasset"),
+                            include_bytes!("../blueprints/hook.uexp"),
+                        )?)
+                    },
+                    &drop,
+                    cutscene,
+                    69,
+                )?;
+                Ok(())
+            });
+        }
+    });
     Ok(())
 }
